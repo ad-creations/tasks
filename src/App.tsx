eb8f8f810d755218e28987e3d94868eff520f883
Alex D <amdaley@udel.edu>
2022-10-27 16:02:46 -0400

    App changes

--- src/App.tsx
+++ src/App.tsx
@@ -13,60 +13,7 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript by Alex Daley
-                <h2> This is for task 3</h2>
-                <img
-                    src="/Users/alexdaley/Desktop/HVAC/HVAC.jpeg"
-                    alt="a picture of an hvac"
-                />
-                <div>
-                    <ol>
-                        <li>One</li>
-                        <li>Two</li>
-                        <li>Three</li>
-                    </ol>
-                </div>
             </header>
-<<<<<<< HEAD
-            <div>
-                <Button onClick={() => console.log("Hello World!")}>
-                    Log Hello World
-                </Button>
-            </div>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
-            <div>
-                <Container>
-                    <Row>
-                        <Col>
-                            Column For Something
-                            <div
-                                style={{
-                                    width: 125,
-                                    height: 125,
-                                    border: "red",
-                                    backgroundColor: "red",
-                                    padding: 100
-                                }}
-                            ></div>
-                        </Col>
-                        <Col>
-                            Column For Something Else
-                            <div
-                                style={{
-                                    width: 125,
-                                    height: 125,
-                                    border: "red",
-                                    backgroundColor: "red",
-                                    padding: 100
-                                }}
-                            ></div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -79,7 +26,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
